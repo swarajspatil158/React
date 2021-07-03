b936658255374f9e3e32070884c80584b7588abd
Swaraj Patil <swarajspatil158@gmail.com>
2021-07-03 11:26:00 +0530

    Live on Github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react'
 import './App.css';
 import Header from "./myComponents/Header";
 import Hello from "./myComponents/Hello";
 
 function App() {
   return (
- <>
+ <div>
  <Header/>
  <Hello/>
- </>
+ </div>
   );
 }
 
